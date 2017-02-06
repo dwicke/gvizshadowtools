aeb371c93ffade1f58b8412932a27247aaa3ab4c
dwicke <drewwicke@gmail.com>
2017-02-06 08:38:57 -0500

    updated for the procserver

--- convert.py
+++ convert.py
@@ -7,24 +7,27 @@ import sys, getopt
 def main(argv):
     simStart = 1200
     simlength = 2400
-    numclients = 40
-    numwebclients = 30
-    numservers = 3
+    numwebclients = 150
+    numbulkclients = 30
+    numprocservers = 10
+    numservers = 15
     try:
-        opts, args = getopt.getopt(argv,"hi:o:s:")
+        opts, args = getopt.getopt(argv,"hi:o:s:p:")
     except getopt.GetoptError:
-       print 'test.py -i <numclientstotal> -o <numwebclients> -s <numservers>'
+       print 'test.py -i <numbulkclients> -o <numwebclients> -s <numservers> -p <numprocservers>'
        sys.exit(2)
     for opt, arg in opts:
-        if opt == '-h':
-            print 'convert.py -i <numclientstotal> -o <numwebclients> -s <numservers>'
+        if opt in ("-h"):
+            print 'convert.py -i <numbulkclients> -o <numwebclients> -s <numservers> -p <numprocservers>'
             sys.exit()
         elif opt in ("-i"):
-            numclients = int(arg)
+            numbulkclients = int(arg)
         elif opt in ("-o"):
             numwebclients = int(arg)
         elif opt in ("-s"):
             numservers = int(arg)
+        elif opt in ("-p"):
+            numprocservers = int(arg)
 
 
     ### The timestamps for the stats.shadow.json do not match the stats.tgen.json log files so don't use this
@@ -48,11 +51,23 @@ def main(argv):
         data = json.load(json_data)
 
         serverbytedata = [0] * simlength * numservers
-        clientbytedata = [0] * simlength * numclients
+        clientbytedata = [0] * simlength * numwebclients
+        bulkclientbytedata = [0] * simlength * numbulkclients
+        procserverbytedata = [0] * simlength * numprocservers
 
         for key, clientdata in data['nodes'].iteritems():
 
-            if "server" in key:
+            if "procserver" in key:
+                serverid = int(key.split("procserver")[1]) - 1
+
+                for clientkey, clientval in data['nodes'][key].iteritems():
+                    for timestamp, bytesArray in data['nodes'][key][clientkey].iteritems():
+                        print(timestamp)
+                        for numBytes in bytesArray:
+                            print(numBytes)
+                            procserverbytedata[int(timestamp) - simStart + serverid * simlength] += int(numBytes)
+
+            elif "server" in key:
                 serverid = int(key.split("server")[1]) - 1
 
                 for clientkey, clientval in data['nodes'][key].iteritems():
@@ -61,17 +76,26 @@ def main(argv):
                         for numBytes in bytesArray:
                             print(numBytes)
                             serverbytedata[int(timestamp) - simStart + serverid * simlength] += int(numBytes)
-            if "client" in key:
+            elif "bulkclient" in key:
+                clientid = int(key.split("bulkclient")[1]) - 1
+                
+                for clientkey, clientval in data['nodes'][key].iteritems():
+                    for timestamp, bytesArray in data['nodes'][key][clientkey].iteritems():
+                        for numBytes in bytesArray:
+                            bulkclientbytedata[int(timestamp) - simStart + clientid * simlength] += int(numBytes)
+            elif "client" in key:
                 clientid = int(key.split("client")[1]) - 1
-                if "bulkclient" in key:
-                    clientid = clientid + numwebclients
                 for clientkey, clientval in data['nodes'][key].iteritems():
                     for timestamp, bytesArray in data['nodes'][key][clientkey].iteritems():
                         for numBytes in bytesArray:
                             clientbytedata[int(timestamp) - simStart + clientid * simlength] += int(numBytes)
 
+
+
         np.savetxt('serverdata.txt', serverbytedata, fmt="%i")
+        np.savetxt('bulkclientdata.txt', bulkclientbytedata, fmt="%i")
         np.savetxt('clientdata.txt', clientbytedata, fmt="%i")
+        np.savetxt('procserverdata.txt', procserverbytedata, fmt="%i")
 
 
 
--- toRPM.py
+++ toRPM.py
@@ -3,21 +3,45 @@
 import json
 import numpy as np
 import sys, getopt
-
+## anomolous clients are label 1
+## regular clients are label 2
 def main(argv):
-	with open('clientdata.txt') as f:
-		td =  open('traindata.txt', 'w')
+	with open('procserverdata.txt') as f:
+		td =  open('rpmprocserverdata.txt', 'w')
 		count = 0
-		clientdata = "1"
+		data = "1"
 		for line in f:
 			count = count + 1
-			clientdata += " " + line.strip("\n")
+			data += " " + line.strip("\n")
 			if (count % 2400) == 0:
-				td.write(clientdata + "\n")
-				clientdata = "1"
+				td.write(data + "\n")
+				data = "1"
+		td.close()
+
+	with open('bulkclientdata.txt') as f:
+		td =  open('rpmbulkclientdata.txt', 'w')
+		count = 0
+		data = "1"
+		for line in f:
+			count = count + 1
+			data += " " + line.strip("\n")
+			if (count % 2400) == 0:
+				td.write(data + "\n")
+				data = "1"
 		td.close()
 
 
+	with open('clientdata.txt') as f:
+		td =  open('rpmclientdata.txt', 'w')
+		count = 0
+		data = "2"
+		for line in f:
+			count = count + 1
+			data += " " + line.strip("\n")
+			if (count % 2400) == 0:
+				td.write(data + "\n")
+				data = "2"
+		td.close()
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
